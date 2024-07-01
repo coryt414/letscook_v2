7e2a602c3ce89f7dca95f60c083b826d23c8a098
Cory Thomas <coryt414@gmail.com>
2024-07-01 16:45:09 -0400

    Update style for ingredient lists

--- letscook_v2/recipes/App.jsx
+++ letscook_v2/recipes/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
-import { Card, Image, Text, Badge, Button, Group, List } from '@mantine/core';
+import { Card, Image, Text, Badge, Button, Group, List, Flex, ThemeIcon } from '@mantine/core';
 
 function App() {
   const [recipes, setRecipes] = useState([]);
@@ -33,18 +33,16 @@ function App() {
     <MantineProvider>
     <div>
     <h1>Recipe Data</h1>
-    <ul>
+          <Flex
+          bg="teal"
+          gap="md"
+          justify="center"
+          align="center"
+          direction="row"
+          wrap="wrap"
+        >
         {recipes.length > 0 ? (
         recipes.map(recipes => (
-            <>
-            <li key={recipes.id}>
-                <strong>{recipes.name}</strong>
-                <ul>
-                    {recipes.ingredients.map(ingredients => (
-                        <li key={ingredients.id}>{ingredients.name}</li>
-                    ))}
-                </ul>
-            </li>
             <Card shadow="sm" padding="lg" radius="md" withBorder>
             <Card.Section>
               <Image
@@ -58,66 +56,31 @@ function App() {
               <Text fw={500}>{recipes.name}</Text>
               <Badge color="pink">On Sale</Badge>
             </Group>
-            
-            <List>
+            <Flex>
+            <List
+                  spacing="xs"
+      size="sm"
+              center
+              icon={
+                <ThemeIcon color="blue" size={6} radius="xl">
+              </ThemeIcon>
+              }>
               {recipes.ingredients.map(ingredients => (
                 <List.Item>{ingredients.name}</List.Item>
               ))}
             </List>
-
-            <Text size="sm" c="dimmed">
-              With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-              activities on and around the fjords of Norway
-            </Text>
-      
+            </Flex>
             <Button color="blue" fullWidth mt="md" radius="md">
               Book classic tour now
             </Button>
           </Card>
-          </>
         ))
     ) : (
         <li>No recipes found</li>
     )}
-    </ul>
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-          height={160}
-          alt="Norway"
-        />
-      </Card.Section>
-
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Norway Fjord Adventures</Text>
-        <Badge color="pink">On Sale</Badge>
-      </Group>
-
-      <Text size="sm" c="dimmed">
-        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-        activities on and around the fjords of Norway
-      </Text>
-
-      <Button color="blue" fullWidth mt="md" radius="md">
-        Book classic tour now
-      </Button>
-    </Card>
-  );
-{/*     <ul>
-        {recipes.map(recipes => (
-        <ul key={recipes.recipe_id}>{recipes.recipe_name}</ul>
-        ))}
-    </ul>
-    <h1>Ingredients</h1>
-      <ul>
-        {ingredients.map(ingredient => (
-          <li key={ingredient.ingredient_id}>{ingredient.ingredient_name}</li>
-        ))}
-      </ul> */}
-    )
-    </div>
-    </MantineProvider>
+    </Flex>
+  </div>
+  </MantineProvider>
   );
 }
 
