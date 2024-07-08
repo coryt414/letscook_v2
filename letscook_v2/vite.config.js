e038aa037e099721abb02a16f7d8ecd33cc1979b
coryt414 <coryt414@gmail.com>
2024-07-08 19:32:41 -0400

    Add dependencies Pages

--- letscook_v2/vite.config.js
+++ letscook_v2/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Pages()],
   build: {
     rollupOptions: {
       input: {
